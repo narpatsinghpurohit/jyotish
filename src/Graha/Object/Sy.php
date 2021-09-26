fedbcba49ae977b3d325a129e748b4bad24230d1
spgunarath <pramuditha@suwahas.com>
2021-09-26 13:31:33 +0530

    Update Sy.php

--- src/Graha/Object/Sy.php
+++ src/Graha/Object/Sy.php
@@ -18,7 +18,7 @@ use Jyotish\Tattva\Ayurveda;
  *
  * @author Kunjara Lila das <vladya108@gmail.com>
  */
-class Sy extends GrahaObject
+class Sy extends GrahaBaseObject 
 {
     /**
      * Abbreviation of the graha.

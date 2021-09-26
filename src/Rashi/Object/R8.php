63c12d995dc074bb27c9d4c70383757dc4e60d0a
spgunarath <pramuditha@suwahas.com>
2021-09-26 13:49:35 +0530

    Update R8.php

--- src/Rashi/Object/R8.php
+++ src/Rashi/Object/R8.php
@@ -17,7 +17,7 @@ use Jyotish\Tattva\Ayurveda;
  *
  * @author Kunjara Lila das <vladya108@gmail.com>
  */
-class R8 extends RashiObject
+class R8 extends RashiBaseObject
 {
     /**
      * Rashi key

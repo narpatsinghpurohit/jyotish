6b943f96aa6f92c6da0c80231dd8344ca5ec393f
spgunarath <pramuditha@suwahas.com>
2021-09-26 13:31:13 +0530

    Update Sk.php

--- src/Graha/Object/Sk.php
+++ src/Graha/Object/Sk.php
@@ -18,7 +18,7 @@ use Jyotish\Tattva\Ayurveda;
  *
  * @author Kunjara Lila das <vladya108@gmail.com>
  */
-class Sk extends GrahaObject
+class Sk extends GrahaBaseObject
 {
     /**
      * Abbreviation of the graha

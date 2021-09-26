c44d1c9141643d1781234a2f11d6e06661c88aa6
spgunarath <pramuditha@suwahas.com>
2021-09-26 13:15:40 +0530

    Update B6.php

--- src/Bhava/Object/B6.php
+++ src/Bhava/Object/B6.php
@@ -13,7 +13,7 @@ use Jyotish\Tattva\Jiva\Nara\Manusha;
  *
  * @author Kunjara Lila das <vladya108@gmail.com>
  */
-class B6 extends BhavaObject
+class B6 extends BhavaBaseObject
 {
     /**
      * Bhava key
